91e93bbf58880f022ecee4b91e3ef8cd8d86a28a
natasha8775735@gmail.com <natalliakhadakouskaya>
2023-01-28 18:59:42 +0300

    HomeWork012

--- HomeWork012/Program.cs
+++ HomeWork012/Program.cs
@@ -4,4 +4,18 @@
 9012 -> 12
 */
 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
+int result = 0;
+
+while(number > 0)
+{
+    int digit = number % 10; // 452 - остаток 2
+    result = result + digit; // 0 + 2 = 2
+    number = number / 10; // убираем хвост числа
+}
+
+Console.WriteLine("Сумма цифр в числе равна: " + result);
+
+// Console.Write("Сумма цифр в числе {0} равна {1}", number, result);
