f91fe4780c6ae2c4b11db6230dde11799b31b20d
natasha8775735@gmail.com <natalliakhadakouskaya>
2023-01-28 15:22:20 +0300

    HomeWork011

--- HomeWork011/Program.cs
+++ HomeWork011/Program.cs
@@ -2,3 +2,23 @@
 3, 5 -> 243 (3 в 5 степени)
 2, 4 -> 16 */
 
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int count = numberB;
+int result = 1;
+
+  for(int i=0; i < count; i++) 
+  {
+   result *= numberA;
+  }
+
+Console.Write("Число A в степени B равно: " + "{0} ^ {1} = ", numberA, count);
+
+
+
+
+Console.WriteLine(result);
