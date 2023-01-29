a919bff7f946fb1d27e7d746a8364cc8ec4cb3d2
natasha8775735@gmail.com <natalliakhadakouskaya>
2023-01-29 15:31:31 +0300

    HomeWork013

--- HomeWork013/Program.cs
+++ HomeWork013/Program.cs
@@ -3,3 +3,39 @@
 6, 1, 33 -> [6, 1, 33]
 */
 
+void FullArray(int[] array) // заполняем массив
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index] = new Random().Next(0, 9);
+        index++;
+    }
+}
+
+void PrintArray(int[] array) // выводим массив 
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        Console.WriteLine(array[index]);
+        index++;
+    }
+
+}
+
+
+Console.WriteLine("Введите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[8]; // создаем новый массив с количеством N-элементов
+FullArray(array);
+Console.Write("Ваш массив: ");
+PrintArray(array);
+
+
+
+
+
+
